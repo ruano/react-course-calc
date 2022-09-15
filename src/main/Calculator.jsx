3cda9bb0abdae548647941cb108a20654f03fbd5
Ruano Martinez Schulze <ruano.martinez@gmail.com>
2022-09-15 17:10:36 -0300

    Implementing logical operations

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -3,24 +3,60 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from "../components/Display";
 
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0, 0],
+    current: 0
+}
+
 export default class Calculator extends Component {
 
+    state = { ...initialState }
+
+    constructor(props) {
+        super(props)
+
+        this.clearMemory = this.clearMemory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigit = this.addDigit.bind(this)
+    }
+
     clearMemory() {
-        console.log('limpar')
+        this.setState({ ...initialState })
     }
 
     setOperation(operation) {
-        console.log(operation)
     }
 
     addDigit(n) {
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')) {
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+
+        const displayValue = currentValue + n
+
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
+        }
     }
 
     render() {
         return (
             <div className="calculator">
-                <Display value={200}></Display>
+                <Display value={this.state.displayValue}></Display>
                 <Button label="AC" click={this.clearMemory} triple />
                 <Button label="/" click={this.setOperation} operation />
                 <Button label="7" click={this.addDigit} />
